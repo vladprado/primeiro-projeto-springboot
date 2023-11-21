b5ef82c9d34f7f1c831fd0f41ddee4cfb31a7a50
vladprado <vladprado.br@gmail.com>
2023-11-20 22:27:42 -0300

    primeiro metodo

--- src/main/java/br/com/vladprado/primeiroprojetospringboot/PrimeiraController.java
+++ src/main/java/br/com/vladprado/primeiroprojetospringboot/PrimeiraController.java
@@ -1,8 +1,17 @@
 package br.com.vladprado.primeiroprojetospringboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @RequestMapping("/primeiraController")
 public class PrimeiraController {
     
+    @GetMapping("/primeiroMetodo")
+    public String primeiroMetodo() {
+        return "Meu primeiro metodo api rest";
+    }
+
 }
