9edbcdcdf91caa76ac1eaa81cd1edcc428c504e3
vladprado <vladprado.br@gmail.com>
2023-11-20 22:17:20 -0300

    primeiro push

---
+++ src/main/java/br/com/vladprado/primeiroprojetospringboot/PrimeiraController.java
@@ -0,0 +1,8 @@
+package br.com.vladprado.primeiroprojetospringboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/primeiraController")
+public class PrimeiraController {
+    
+}
---
+++ src/test/java/br/com/vladprado/primeiroprojetospringboot/PrimeiroProjetoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vladprado.primeiroprojetospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
