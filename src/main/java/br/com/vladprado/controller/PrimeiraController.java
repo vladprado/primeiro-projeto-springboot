2fb86616a9be068ee120e2c7fbf0ac10ba3425a0
vladprado <vladprado.br@gmail.com>
2023-11-22 09:39:47 -0300

    ComponentScan annotation

--- src/main/java/br/com/vladprado/controller/PrimeiraController.java
+++ src/main/java/br/com/vladprado/controller/PrimeiraController.java
@@ -1,4 +1,4 @@
-package br.com.vladprado.primeiroprojetospringboot;
+package br.com.vladprado.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +11,7 @@ public class PrimeiraController {
     
     @GetMapping("/primeiroMetodo")
     public String primeiroMetodo() {
-        return "Meu primeiro metodo api rest";
+        return "Parabens, voce criou sua primeira rota";
     }
 
 }
--- src/main/java/br/com/vladprado/primeiroprojetospringboot/PrimeiroProjetoSpringbootApplication.java
+++ src/main/java/br/com/vladprado/primeiroprojetospringboot/PrimeiroProjetoSpringbootApplication.java
@@ -2,8 +2,10 @@ package br.com.vladprado.primeiroprojetospringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.vladprado")
 public class PrimeiroProjetoSpringbootApplication {
 
 	public static void main(String[] args) {
