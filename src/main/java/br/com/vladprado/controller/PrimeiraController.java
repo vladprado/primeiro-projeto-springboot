d44187e658528cea39fcdaf8d28721be7763796a
vladprado <vladprado.br@gmail.com>
2023-11-22 10:03:09 -0300

    Param types

--- src/main/java/br/com/vladprado/controller/PrimeiraController.java
+++ src/main/java/br/com/vladprado/controller/PrimeiraController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +37,16 @@ public class PrimeiraController {
     @PostMapping("/metodoComBodyParams")
     public String metodoComBodyParams(@RequestBody Usuario usuario) {
         return "metodoComBodyParams " + usuario.username;
+    }
+
+    @PostMapping("/metodoComHeaders")
+    public String metodoComHeaders(@RequestHeader("name") String name) {
+        return "metodoComHeaders " + name;
+    }
+
+    @PostMapping("/metodoComListHeaders")
+    public String metodoComListHeaders(@RequestHeader Map<String, String> headers) {
+        return "metodoComListHeaders " + headers.entrySet();
     }
 
     record Usuario(String username) {}
